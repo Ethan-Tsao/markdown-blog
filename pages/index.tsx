610a612c5bf2fdde4944cf8f4c11c823a7754427
Tsao, Ethan (et475a) <et475a@att.com>
2021-07-14 10:05:25 -0500

    refactored About page into component; edited home page; added new portraits

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,9 +9,14 @@ import {
   LinkBox,
   LinkOverlay,
   Flex,
+  Image,
+  AspectRatio,
 } from "@chakra-ui/react";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 import Navbar from "components/Navbar";
 import { HTMLMotionProps, motion } from "framer-motion";
+
+export const MotionChevron = motion(ChevronDownIcon);
 
 const Home = () => {
   const mode = useColorModeValue("solarizedDark.600", "solarizedLight.500");
@@ -24,72 +29,99 @@ const Home = () => {
       align="center"
       color={mode}
     >
-      {/* Heading */}
       <Navbar />
-      <Box alignContent="center">
-        <Heading textAlign="left" size="4xl" mt="5rem">
-          Hi, I&apos;m Ethan
-        </Heading>
-        <Heading textAlign="left" size="xl">
-          You don&apos;t know me yet but you will soon
-        </Heading>
+      {/* Heading */}
+      <SimpleGrid columns={{ base: 1, xl: 2 }} h="80vh">
+        <Box align="center" maxH="50%">
+          <Heading
+            textAlign="left"
+            size="4xl"
+            mt={{ base: "2rem", md: "5rem" }}
+          >
+            Hi, I&apos;m Ethan
+          </Heading>
+          <Heading textAlign="left" size="xl">
+            You don&apos;t know me yet but you will soon
+          </Heading>
+        </Box>
+        <Box>
+          <AspectRatio ratio={3 / 4} maxW="90%">
+            <Image src="images/home/me.jpg" objectFit="cover" />
+          </AspectRatio>
+        </Box>
+      </SimpleGrid>
+      <Box justifyItems="center" h="20vh">
+        <MotionChevron
+          h="10"
+          w="10"
+          animate={{
+            y: [-10, 10, -10],
+          }}
+          transition={{
+            duration: 1,
+            repeat: Infinity,
+          }}
+        />
       </Box>
 
       {/* <Divider /> */}
 
       {/* Testimonials */}
-      <Heading textAlign="center" size="2xl" mt="5rem">
-        My Testimonials
-      </Heading>
-      <Stack
-        align={"center"}
-        justifyContent={"space-between"}
-        direction={{ base: "column", md: "row" }}
-        spacing="3em"
-        my={5}
-      >
+      <Box h="100vh">
+        <Heading textAlign="center" size="2xl" my="2rem">
+          My Testimonials
+        </Heading>
         <Stack
-          dir={"column"}
-          rounded={"xl"}
-          bg={bgMode}
-          p={6}
-          h={{ base: "12rem", sm: "20rem" }}
-          w={{ base: "100%", sm: "30rem" }}
+          align={"center"}
+          justifyContent={{ base: "center", xl: "space-between" }}
+          justify="center"
+          direction={{ base: "column", md: "row" }}
+          spacing="3em"
+          my={5}
         >
-          <Text fontSize={{ base: "xl", md: "2xl" }}>
-            He&apos;s the hardest working and most diligent person I&apos;ve
-            ever met. Definitely my favorite child.
-          </Text>
-          <Text py={"2rem"}>- My Mom (probably(not))</Text>
+          <Stack
+            dir={"column"}
+            rounded={"xl"}
+            bg={bgMode}
+            p={6}
+            h={{ base: "12rem", sm: "20rem" }}
+            w={{ base: "100%", sm: "30rem" }}
+          >
+            <Text fontSize={{ base: "xl", md: "2xl" }}>
+              He&apos;s the hardest working and most diligent person I&apos;ve
+              ever met. Definitely my favorite child.
+            </Text>
+            <Text py={"1rem"}>- My Mom (probably(not))</Text>
+          </Stack>
+          <Stack
+            dir={"column"}
+            rounded={"xl"}
+            bg={bgMode}
+            p={6}
+            h={{ base: "12rem", sm: "20rem" }}
+            w={{ base: "100%", sm: "30rem" }}
+          >
+            <Text fontSize={{ base: "xl", md: "2xl" }}>
+              Honestly, I&apos;ve never met anyone funnier. His memes are top
+              notch and he&apos;s always quick with a joke
+            </Text>
+            <Text py={"1rem"}>- My best friend (definitely)</Text>
+          </Stack>
+          <Stack
+            dir={"column"}
+            rounded={"xl"}
+            bg={bgMode}
+            p={6}
+            h={{ base: "12rem", sm: "20rem" }}
+            w={{ base: "100%", sm: "30rem" }}
+          >
+            <Text fontSize={{ base: "xl", md: "2xl" }}>
+              Who the hell are you?
+            </Text>
+            <Text py={"1rem"}>- Some random guy I met at McDonalds</Text>
+          </Stack>
         </Stack>
-        <Stack
-          dir={"column"}
-          rounded={"xl"}
-          bg={bgMode}
-          p={6}
-          h={{ base: "12rem", sm: "20rem" }}
-          w={{ base: "100%", sm: "30rem" }}
-        >
-          <Text fontSize={{ base: "xl", md: "2xl" }}>
-            Honestly, I&apos;ve never met anyone funnier. His memes are top
-            notch and he&apos;s always quick with a joke
-          </Text>
-          <Text py={"2rem"}>- My best friend (definitely)</Text>
-        </Stack>
-        <Stack
-          dir={"column"}
-          rounded={"xl"}
-          bg={bgMode}
-          p={6}
-          h={{ base: "12rem", sm: "20rem" }}
-          w={{ base: "100%", sm: "30rem" }}
-        >
-          <Text fontSize={{ base: "xl", md: "2xl" }}>
-            Who the hell are you?
-          </Text>
-          <Text py={"2rem"}>- Some random guy I met at McDonalds</Text>
-        </Stack>
-      </Stack>
+      </Box>
     </Box>
   );
 };
