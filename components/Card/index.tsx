ece00033562e790a0d1d3ca35ea5d1809115f678
Tsao, Ethan (et475a) <et475a@att.com>
2021-07-14 16:07:53 -0500

    changed the way links work on the card component

--- components/Card/index.tsx
+++ components/Card/index.tsx
@@ -35,92 +35,76 @@ const BlogCard: React.FC<IIPost> = ({ post }: IIPost) => {
   const router = useRouter();
   const route = router.pathname;
   return (
-    <Link href={`${route}/${post.slug}`}>
-      <LinkBox minWidth="100%">
-        <LinkOverlay>
-          <Box
-            marginTop={{ base: "1", sm: "5" }}
-            display="flex"
-            flexDirection={{ base: "column", md: "row" }}
-            justifyContent="center"
-            rounded={12}
-            boxShadow="2xl"
-            paddingBottom={{ base: "1", sm: "5" }}
-            bg={bgMode}
-            color={mode}
-            minWidth="100%"
-            h={{ base: "flex", xl: "30rem" }}
-          >
-            {/* blog post image box */}
-            <Box
-              display="flex"
-              flex="1"
-              position="relative"
-              alignItems="center"
+    <Box
+      marginTop={{ base: "1", sm: "5" }}
+      display="flex"
+      flexDirection={{ base: "column", md: "row" }}
+      justifyContent="center"
+      rounded={12}
+      boxShadow="2xl"
+      paddingBottom={{ base: "1", sm: "5" }}
+      bg={bgMode}
+      color={mode}
+      minWidth="100%"
+      h={{ base: "flex", xl: "30rem" }}
+    >
+      {/* blog post image box */}
+      <Box display="flex" flex="1" position="relative" alignItems="center">
+        <Box
+          w={{ base: "100%", sm: "85%" }}
+          zIndex="2"
+          marginLeft={{ base: "0", sm: "5%" }}
+          marginTop="5%"
+        >
+          {post.frontmatter.video != "none" && (
+            <AspectRatio ratio={16 / 9}>
+              <iframe src={post.frontmatter.video} />
+            </AspectRatio>
+          )}
+          {post.frontmatter.cover_image != "none" && (
+            <Image
+              borderRadius="lg"
+              src={post.frontmatter.cover_image}
+              alt="some good alt text"
+              objectFit="contain"
+              w={{ base: "95%" }}
+            />
+          )}
+        </Box>
+      </Box>
+      {/* blog info box */}
+      <Box
+        display="flex"
+        flex="1"
+        flexDirection="column"
+        alignContent={{ base: "center", md: "left" }}
+        marginTop={10}
+        marginRight={3}
+      >
+        <LinkBox>
+          <LinkOverlay>
+            <Heading
+              marginTop="1"
+              textAlign={{ base: "center", md: "left" }}
+              size="2xl"
             >
-              <Box
-                w={{ base: "100%", sm: "85%" }}
-                zIndex="2"
-                marginLeft={{ base: "0", sm: "5%" }}
-                marginTop="5%"
-              >
-                <Link href={`${route}/${post.slug}`}>
-                  <LinkBox>
-                    <LinkOverlay>
-                      {post.frontmatter.video != "none" && (
-                        <AspectRatio ratio={16 / 9}>
-                          <iframe src={post.frontmatter.video} />
-                        </AspectRatio>
-                      )}
-                      {post.frontmatter.cover_image != "none" && (
-                        <Image
-                          borderRadius="lg"
-                          src={post.frontmatter.cover_image}
-                          alt="some good alt text"
-                          objectFit="contain"
-                          w={{ base: "95%" }}
-                        />
-                      )}
-                    </LinkOverlay>
-                  </LinkBox>
-                </Link>
-              </Box>
-            </Box>
-            {/* blog info box */}
-            <Box
-              display="flex"
-              flex="1"
-              flexDirection="column"
-              alignContent={{ base: "center", md: "left" }}
-              marginTop={10}
-              marginRight={3}
-            >
-              <Link href={`${route}/${post.slug}`}>
-                <LinkBox>
-                  <LinkOverlay>
-                    <Heading
-                      marginTop="1"
-                      textAlign={{ base: "center", md: "left" }}
-                      size="2xl"
-                    >
-                      {post.frontmatter.title}
-                    </Heading>
-                  </LinkOverlay>
-                </LinkBox>
+              <Link href={`${route}/${post.slug}`} passHref>
+                {post.frontmatter.title}
               </Link>
-              <Text
-                as="p"
-                my="2"
-                fontSize={["md", "lg", "xl", "2xl"]}
-                textAlign={{ base: "center", md: "left" }}
-              >
-                {post.frontmatter.excerpt}
-              </Text>
-            </Box>
-          </Box>
-        </LinkOverlay>
-      </LinkBox>
-    </Link>
+            </Heading>
+          </LinkOverlay>
+        </LinkBox>
+
+        <Text
+          as="p"
+          my="2"
+          fontSize={["md", "lg", "xl", "2xl"]}
+          textAlign={{ base: "center", md: "left" }}
+        >
+          {post.frontmatter.excerpt}
+        </Text>
+      </Box>
+    </Box>
   );
 };
 
