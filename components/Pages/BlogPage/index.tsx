e31ee7511a2223b6a7a4e62c17f09aef6b95575f
Tsao, Ethan (et475a) <et475a@att.com>
2021-07-21 11:30:33 -0500

    tried then removed aos

--- components/Pages/BlogPage/index.tsx
+++ components/Pages/BlogPage/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import BlogCard from "components/Card";
 import { Navbar } from "components/Navbar";
 import { Box, Flex, Heading, useColorModeValue } from "@chakra-ui/react";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "theme";
 import { AppProps } from "next/app";
-import "@fontsource/caveat";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000,
+    });
+  }, []);
   return (
     <ChakraProvider theme={theme}>
       <Component {...pageProps} />
